722a689c367a3d67aa210dcf73a6822e3b042064
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-09-28 14:45:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const db = require('quick.db');
 const config = require('./config.json');
+const TOKEN = "";
 
 const { cmd_prefix, ownerID } = config;
 
@@ -38,4 +39,4 @@ client.on('message', async message => {
 });
 
 
-client.login(proccess.env.BOT_TOKEN);
+client.login(TOKEN);
