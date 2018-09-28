e411f256f5582c5a7607da7a9b537a36e37ff03d
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-09-28 17:00:53 +0800

    Create cat.js

---
+++ commands/cat.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const superagent = require('superagent');
+
+module.exports.run = async (client, message, args) => {
+  let { body } = await superagent
+  .get("https://nekos.life/api/v2/img/meow");
+  
+  let embed = new Discord.RichEmbed()
+  .setAuthor("Random cat found!")
+  .setFooter("Powered by nekos.life")
+  .setImage(body.url);
+  
+  message.channel.send(embed);
+}
+
+module.exports.help = {
+  name: "cat"
+}
