454559cc6f8104dcdd648de608cfeda98d5e4743
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-09-29 20:43:45 +0800

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -2,12 +2,15 @@ const Discord = require('discord.js');
 const config = require('../config.json')
 
 module.exports.run = async (client, message, args) => {
-  if(message.author.id !== config.ownerID) {
+  try{
+    if(message.author.id !== config.ownerID) {
     message.channel.send("Successfully reload!");
     client.destroy();
+    return;
+  } else {
+    message.channel.send("You don't my developer!")
   }
-  
-  message.channel.send("You dont my dev!")
+  }
 }
 
 module.exports.help = {
