e0b87522efe29a603c3d943c864bff48e90c71c1
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-09-29 20:55:46 +0800

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,6 +3,15 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
   let txt = args.join(' ');
   if(!args[0]) return message.channel.send("Pls provide text!");
+  if(args[0] === "@everyone") {
+    message.channel.send("Pls don't use this word!")
+    return;
+  }
+  
+  if(args[0] === "@here") {
+    message.channel.send("Pls don't use this word!")
+    return;
+  }
   
   message.channel.send(txt);
 }
