eb953708329872a6a96ef98baf9277a15cd9edb7
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-09-29 20:19:52 +0800

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -3,11 +3,11 @@ const config = require('../config.json')
 
 module.exports.run = async (client, message, args) => {
   if(message.author.id === config.ownerID) {
-    client.destroy();
     let msg = message.channel.send("``Reloading...``");
     let msg2 = await msg.delete({
       time: 2000
     });
+    client.destroy();
     msg2.edit("Succefully reload!");
   }
   
