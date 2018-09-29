d7ad8bcd8fb869d12ac6026eef8e873ecbedec97
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-09-29 20:29:21 +0800

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const config = require('../config.json')
 
 module.exports.run = async (client, message, args) => {
-  if(message.author.id === config.ownerID) {
+  if(!message.author.id === config.ownerID) {
     let msg = message.channel.send("``Reloading...``");
     await msg.delete({
       time: 30
