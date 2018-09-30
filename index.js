978f6753f802941436373c1e881a20d97c993966
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-09-30 20:46:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs');
+const db = require('quick.db');
 const config = require('./config.json');
 
 const { cmd_prefix, ownerID } = config;
