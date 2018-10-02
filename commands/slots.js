4f7a8bf303f58ee6b74781a62de2eca20d67c2cb
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-10-02 21:31:24 +0800

    Create slots.js

---
+++ commands/slots.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+  machine = ["🍏", "🍉", "🍇", "🍓"];
+  random = Math.floor(Math.random() * machine.length);
+
+  let embed = new Discord.RichEmbed()
+  .setAuthor("**[🎰 SLOT MACHINE 🎰]**")
+  .setFooter(author + " started slot machine!")
+  .setTimestamp()
+  .addField(` `, `${machine[random]} | ${machine[random]} | ${machine[random]}`)
+  .addField(` `, `${machine[random]} | ${machine[random]} | ${machine[random]}`)
+  .addField(` `, `${machine[random]} | ${machine[random]} | ${machine[random]}`);
+  
+  message.channel.send(embed);
+}
+
+module.exports.help = {
+  name: "slots"
+}
