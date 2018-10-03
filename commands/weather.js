f8de246e77870b42ea079b0825ddceb500af4cbb
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-10-03 08:48:46 +0800

    Create weather.js

---
+++ commands/weather.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const weather = require('weather-js');
+const color = require('../utils/color.json');
+
+module.exports.run = async (client, message, args) => {
+  let txt = args.join(' ');
+  if(!args[0]) return message.channel.send("Please enter the correct location!")
+  
+  weather.find({search: txt, degreeType: "F"}, (err, result) {
+    if(err) message.channel.send("Please enter the correct location!");
+    
+    let current = result[0].current;
+    let location = result[0].location;
+    
+    let embed = new Discord.RichEmbed()
+    .setDescription(`**${current.skytext}**`)
+    .setAuthor(`Weather for ${current.observationpoint}`)
+    .setThumbnail(current.imageUrl)
+    .setColor(color.gold)
+    .addField("Timezone", `UTC${location.timezone}`, true)
+    .addField("Degree Type", location.degreetype, true)
+    .addField("Temperature", `${location.temperature} Degrees`, true)
+    .addField("Feels Like", `${current.feelslike} Degrees`, true)
+    .addField("Winds", current.winddisplay, true)
+    .addField("Humidity", `${current.humidity}%`, true);
+    
+    message.channel.send(embed);
+  });
+}
+
+module.exports.help = {
+  name: "weather"
+}
