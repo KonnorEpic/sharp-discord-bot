2ef58b2f51f78d62ca6d355ad35d59b87ca8a246
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-10-03 12:01:44 +0800

    Update choose.js

--- commands/choose.js
+++ commands/choose.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-module.exports.run = async (client, message, args) => {\
+module.exports.run = async (client, message, args) => {
   let choose = args.join(' ').split(" | ");
   if(!args[0]) return message.channel.send("Pls provide <text1 | text2> or more.");
   let random = Math.floor(Math.random() * choose.length);
