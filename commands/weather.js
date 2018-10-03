6c4e4066768eb3ef5c26f1dc5507d95857ae52d7
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-10-03 09:00:57 +0800

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -4,10 +4,15 @@ const color = require('../utils/color.json');
 
 module.exports.run = async (client, message, args) => {
   let txt = args.join(' ');
-  if(!args[0]) return message.channel.send("Please enter the correct location!");
+  if(!args[0]) return message.channel.send("Please enter the location!");
   
   weather.find({search: txt, degreeType: "F"}, function(err, result) {
-    if(err) message.channel.send("Please enter the correct location!");
+    if(err) message.channel.send("You don't enter location!");
+    
+    if(result.length === 0) {
+      message.channel.send("**Pls enter a valid location**!");
+      return;
+    }
     
     let current = result[0].current;
     let location = result[0].location;
