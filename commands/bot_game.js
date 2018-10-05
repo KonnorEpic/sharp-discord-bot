225d5657803e748d898fd8ae7894c823ef4a8384
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-10-05 20:01:36 +0800

    Update bot_game.js

--- commands/bot_game.js
+++ commands/bot_game.js
@@ -4,6 +4,7 @@ const superagent = require('superagent');
 module.exports.run = async (client, message, args) => {
   if(message.author.id !== "416540213696004117") return;
   let txt = args.join(' ');
+  if(!args[0]) return message.channel.send("Owner, provide text.")
   
   client.user.setActivity(txt, {type: "PLAYING"});
 }
