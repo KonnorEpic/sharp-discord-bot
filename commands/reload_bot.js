f5da7fd303a67918fd3bdf067414e6606abdca81
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-10-05 19:18:08 +0800

    Update reload_bot.js

--- commands/reload_bot.js
+++ commands/reload_bot.js
@@ -4,6 +4,7 @@ const config = require('../config.json');
 module.exports.run = async (client, message, args) => {
   if(message.author.id !== config.ownerID) return message.channel.send("No, no, no.");
   client.destroy();
+  message.channel.send("Successfully reload!");
 }
 
 module.exports.help = {
