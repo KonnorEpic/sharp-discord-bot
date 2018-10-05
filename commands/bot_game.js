daacebe624f8a522836e7ccf5c2549d3208f99d9
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-10-05 20:10:43 +0800

    Update bot_game.js

--- commands/bot_game.js
+++ commands/bot_game.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const superagent = require('superagent');
 
 module.exports.run = async (client, message, args) => {
   if(message.author.id !== "416540213696004117") return;
