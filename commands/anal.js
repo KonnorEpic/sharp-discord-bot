1f30a5b26915ee5bacd879c463b891d75d7a46c7
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-10-18 09:32:46 +0800

    Update anal.js

--- commands/anal.js
+++ commands/anal.js
@@ -3,7 +3,14 @@ const nekoClient = require('nekos.life');
 const neko = new nekoClient();
 
 module.exports.run = async (client, message, args) => {
-  if(!message.channel.nsfw) return message.channel.send("This is not **NSFW** channel!");
+  let nsfwEmbed = new Discord.RichEmbed()
+  .setAuthor("This is not NSFW channel!")
+  .setDescription("Use **NSFW** commands in **NSFW** channel (look in channel settings)")
+  .setImage("https://i.imgur.com/oe4iK5i.gif")
+  .setFooter(message.author.username)
+  .setTimestamp();
+  
+  if(!message.channel.nsfw) return message.channel.send(nsfwEmbed);
   
   let anal = await neko.getNSFWAnal();
 
