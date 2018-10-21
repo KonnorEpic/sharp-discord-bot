748ea300f0bcef2214942a41cf044846f847da82
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-10-21 14:58:56 +0800

    Update rip.js

--- commands/rip.js
+++ commands/rip.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext('2d')
 
 module.exports.run = async (client, message, args) => {
   let user = message.mentions.users.first() || message.author;
-  const background = await loadImage('../assets/rip.jpg');
+  const background = await loadImage('https://github.com/KonnorEpic/sharp-discord-bot/blob/master/assets/rip.jpg');
   const ava = await loadImage(user.displayAvatarURL);
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(ava, 80, 150, 65, 65);
