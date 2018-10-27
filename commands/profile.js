e107988a5e8e32b227653ec08c977663b01a4763
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-10-27 11:44:21 +0800

    Create profile.js

---
+++ commands/profile.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const sql = require('sqlite');
+
+module.exports.run = async (client, message, args) => {
+  
+}
+
+module.exports.help = {
+  name: "profile"
+}
