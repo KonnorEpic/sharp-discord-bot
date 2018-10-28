3feb72a70b542a39c973f179b7f5ae6bfa3bfa13
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-10-28 11:14:48 +0800

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -2,7 +2,32 @@ const Discord = require('discord.js');
 const sql = require('sqlite');
 
 module.exports.run = async (client, message, args) => {
+  sql.open('../assets/sqlite/descriptions.sqlite');
   
+  if(args[0] === 'desc:') {
+    let text = args.slice(6).join(' ');
+    if(!text[0]) return message.channel.send('You description provided!');
+    
+    sql.get(`SELECT * FROM descriptions WHERE userId = "${message.author.id}"`).then(row => {
+      if(!row) {
+        sql.run("INSERT INTO descriptions (userId, desc) VALUES (?, ?)", [message.author.id, text]);
+      } else {
+        sql.run(`UPDATE descriptions SET desc = "${text}" WHERE userId = ${message.author.id}`);
+      }
+    }).catch(() => {
+      console.error;
+      sql.run("CREATE TABLE IF NOT EXISTS descriptions (userId TEXT, desc TEXT)").then(() => {
+        sql.run("INSERT INTO descriptions (userId, desc) VALUES (?, ?)", [message.author.id, text]);
+      }):
+    });
+    message.channel.send('Successfully description changed!');
+  }
+  
+  let embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setFooter(`${message.author.username}'s profile`);
+  
+  message.channel.send(embed);
 }
 
 module.exports.help = {
